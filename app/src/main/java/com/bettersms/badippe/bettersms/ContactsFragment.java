94f91b91ab6326d99a6d9e502855b650892cda94
Philippe Antoine <antoine.philippe62@gmail.com>
2015-09-25 11:32:26 +0200

    debut fragment contacts

--- app/src/main/java/com/bettersms/badippe/bettersms/ContactsFragment.java
+++ app/src/main/java/com/bettersms/badippe/bettersms/ContactsFragment.java
@@ -1,12 +1,22 @@
 package com.bettersms.badippe.bettersms;
 
 import android.app.Fragment;
+import android.database.Cursor;
+import android.os.Bundle;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.Loader;
 import android.view.LayoutInflater;
+import android.provider.ContactsContract;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
 
 /**
  * Created by philippe on 24/09/15.
  */
-public class ContactsFragment extends Fragment {
+public class ContactsFragment extends Fragment implements
+        LoaderManager.LoaderCallbacks<Cursor>,
+        AdapterView.OnItemClickListener {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle saveInstanceState){
@@ -15,4 +25,24 @@ public class ContactsFragment extends Fragment {
     }
 
 
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+    }
+
+
+    @Override
+    public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+        return null;
+    }
+
+    @Override
+    public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
+
+    }
+
+    @Override
+    public void onLoaderReset(Loader<Cursor> loader) {
+
+    }
 }
